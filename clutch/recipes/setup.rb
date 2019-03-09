6c81713ffd382bd369b58a032690f172e9b0fd6c
Brandon Lee Kitajchuk <kitajchuk@gmail.com>
2019-03-08 16:08:40 -0800

    Update setup.rb

--- clutch/recipes/setup.rb
+++ clutch/recipes/setup.rb
@@ -123,8 +123,7 @@ commands = [
     "mv /node-#{node_version}-linux-x64/lib/node_modules /usr/local/lib/node_modules",
     "rm -rf /node-#{node_version}-linux-x64.tar.xz",
     "rm -rf /node-#{node_version}-linux-x64",
-    "npm install npm@#{npm_version} -g",
-    "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000"
+    "npm install npm@#{npm_version} -g"
 ]
 
 commands.each do |com|
